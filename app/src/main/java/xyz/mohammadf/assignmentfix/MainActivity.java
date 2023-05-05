7ae2dde1a705427ef114b8641b03a62707cd3874
mohammad <faraj4497@gmail.com>
2023-05-05 11:57:13 +0300

    V2

--- app/src/main/java/xyz/mohammadf/assignmentfix/MainActivity.java
+++ app/src/main/java/xyz/mohammadf/assignmentfix/MainActivity.java
@@ -1,8 +1,12 @@
 package xyz.mohammadf.assignmentfix;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -17,29 +21,99 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
         greetingText = findViewById(R.id.greetingText);
 
         User user = new User();
-
 
         Calendar cal = Calendar.getInstance();
         int hour = cal.get(Calendar.HOUR_OF_DAY);
         if(hour < 6 || hour > 18){
             String text = "Good Evening, " + user.getfName() + " " + user.getlName() + "\uD83C\uDF19";
-            greetingText.setText(text);
+            //greetingText.setText(text);
         } else {
             String text = "Good Morning, " + user.getfName() + " " + user.getlName() + "☀️";
-            greetingText.setText(text);
+            //greetingText.setText(text);
         }
-
-
-
+        System.out.println("test");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    public void billClick(View view) {
 
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage("Will added next update.");
+        builder1.setCancelable(true);
 
+        builder1.setPositiveButton(
+                "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+
+    }
+
+    public void taskClick(View view) {
+
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage("Will added next update.");
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+
+        
+    }
+
+    public void examClick(View view) {
+
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage("Will added next update.");
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+
+    }
+
+    public void gradesClick(View view) {
+
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage("Will added next update.");
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+
+    }
 
 }
